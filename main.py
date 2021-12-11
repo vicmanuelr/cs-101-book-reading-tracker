2540da8a1f99acb8b6912248298b084f3ba8dc72
Victor Ponce <vicmanuelr@gmail.com>
2021-12-11 13:03:26 -0600

    INITIAL COMMIT

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 # TODO 1: USERNAME AND FILE SETUP FOR DATABASE AND KEEP TRACK OF BOOK READING
+def username_setup():
+    try:
+        with open("database.txt") as file:
+            database_lines = file.readlines()
+    except FileNotFoundError:
+        with open("database.txt", "w") as file:
+            database_lines = []
+    finally:
+        user = input("Please enter your username to start:")
+        if user in database_lines:
+            print("User already exist")
+        else:
+            database_lines.append(user)
+
 
 # TODO 2: GOALS FOR THE USER EG//BOOKS PER YEAR
 
