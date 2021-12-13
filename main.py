ccdb11282d5c4d4c66be7e415d3afbeb62c13ae7
Victor Ponce <vicmanuelr@gmail.com>
2021-12-13 17:25:01 -0600

    username_setup function defined and tested

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 # TODO 1: USERNAME AND FILE SETUP FOR DATABASE AND KEEP TRACK OF BOOK READING
-def username_setup():
-    try:
-        with open("database.txt") as file:
-            database_lines = file.readlines()
-    except FileNotFoundError:
-        with open("database.txt", "w") as file:
-            database_lines = []
-    finally:
-        user = input("Please enter your username to start:")
-        if user in database_lines:
-            print("User already exist")
-        else:
-            database_lines.append(user)
+USERSDB = {"victor": {"first_name": "Victor", "last_name": "Ponce"}}
 
+def username_setup():
+    """
+    Checks the user setup status, if it's a user in DATABASE or needs to be added.
+    """
+    user = input("Please enter your username: ")
+    global USERSDB
+    if user in USERSDB:
+        return f"Welcome to book tracker again {USERSDB[user]['first_name']}"
+    else:
+        first_name = input("It looks like you have not created your account, please enter your first name: ")
+        last_name = input("Please enter your last name: ")
+        username = input("Please confirm your username: ")
+        USERSDB[username] = {"first_name": first_name, "last_name": last_name}
+        print(f"Welcome to book tracker {USERSDB[user]['first_name']}")
+
+# test the user creation
+# print(username_setup())
 
 # TODO 2: GOALS FOR THE USER EG//BOOKS PER YEAR
+#
+def define_goals(user):
+    pass
+
 
 # TODO 3: TRACK DAILY PROGRESS (BOOK TITLE, PAGES, HOURS)
 
